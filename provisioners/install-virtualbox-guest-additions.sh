f6a818621415ea199d7d36a3e61ca325374aefe6
Michael Chapman <woppin@gmail.com>
2014-08-11 01:23:23 +1000

    Account for older (< 4.3.8) VBox version

--- provisioners/install-virtualbox-guest-additions.sh
+++ provisioners/install-virtualbox-guest-additions.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+KERN_DIR=/usr/src/kernels/`uname -r`
+export KERN_DIR
+export MAKE='/usr/bin/gmake -i'
+
 # Mount the disk image
 cd /tmp
 mkdir /tmp/isomount
 mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /tmp/isomount
 
 # Install the drivers
-/tmp/isomount/VBoxLinuxAdditions.run
+/tmp/isomount/VBoxLinuxAdditions.run --nox11
 
 # Cleanup
 umount isomount
